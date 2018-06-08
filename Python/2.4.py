d78742a6c05e74047f7da8f44beb2e4292db82f8
karlittocute <kapitalina.ignatova@gmail.com>
2018-06-08 04:11:40 +0300

    Revert "Та дааа!"
    This reverts commit f9a551980c66843dc835662d975da1a358208ed6.

--- Python/2.4.py
+++
@@ -1,9 +0,0 @@
-# Кицела Каролина ИВТ 3 курс  
-# Вариант 6 
-# Найти сумму всех чисел с плавающей точкой
-
-b = ("name",	"	DeLorean	DMC-12",	"motor_pos",	"rear",	"n_of_wheels",	4,
-"n_of_passengers",	2,	"weight",	1.230,	"height",	1.140,	"length",	4.216,
- "width", 1.857, "max_speed", 177)
-
-print sum(b[9:16:2])
